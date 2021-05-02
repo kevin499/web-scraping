6530ead92cf3311d6585e7c78b78202056995fdb
Kevin <33708864+kevin499@users.noreply.github.com>
2021-05-02 03:03:31 -0300

    creating new search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* Main */}
+
+      <section className="section-search">
+        <div className="main-title">
+          <h1>Encontrá el mejor trabajo para ti</h1>
+          <p>Mejores trabajos</p>
+        </div>
+        <form action="">
+          <div className="search search-word">
+            <input type="text" placeholder="Empresa / e / e" />
+          </div>
+          <div className="search search-location">
+            <input type="text" placeholder="Ubicacion" />
+          </div>
+          <button className="search-button">Buscar</button>
+        </form>
+
+      </section>
+
+      {/* Search box */}
+
     </div>
   );
 }
